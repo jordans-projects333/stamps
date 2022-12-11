b4e3526b263a6e2eb9a15686ca07e0d93933d31b
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-12-11 17:41:46 +0000

    first commit

--- server.js
+++ server.js
@@ -10,28 +10,9 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', (socket) => {
-    // Send data to user
-    socket.emit('message', 'welcome')
-
-    // Broadcast when user connects(emit to everyone except user)
-    socket.broadcast.emit('message', "l")
-
-    // Runs when user disconnects
-    socket.on('disconnect', () => {
-        // Send data to everyone
-        io.emit('message', "s")
-    })
-
-    // Listen for chat message
-    socket.on('chatMessage', (msg) => {
-        socket.broadcast.emit('point', msg)
-    })
-    
-})
 
 
 
 const PORT = 3000
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
